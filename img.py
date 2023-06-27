c5926c96e8aee0551a436fbeab99693a5c42e21f
Rahul-Adidravid <85445739+Rahul-Adidravid@users.noreply.github.com>
2023-06-27 14:07:50 +0530

    img.py

--- img.py
+++ img.py
@@ -3,7 +3,7 @@ import torchvision.transforms as T
 from PIL import Image
 
 # Load the pre-trained YOLOv5 model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+, 'yolov5s', pretrained=True)
 
 # Load and preprocess the image
 image_path = r'C:\Users\AJ\Downloads\pexels-alexandra-novitskaya-2951921'
